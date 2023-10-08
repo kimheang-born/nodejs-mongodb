58edabb4c1a36aea2c3421208f963e42bb7fc36f
Kimheang <kimheang135@gmail.com>
2023-10-08 21:16:28 +0700

    first commit

---
+++ models/user.js
@@ -0,0 +1,96 @@
+const { ObjectId } = require('mongodb');
+const { getDb } = require('../util/database');
+
+class User {
+  constructor(username, email, cart, id) {
+    this.username = username;
+    this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    if (!this.cart || !this.cart.items) {
+      this.cart = { items: [] };
+    }
+    const cartProductIndex = this.cart.items.findIndex(
+      (cp) => cp.productId.toString() === product._id.toString()
+    );
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
+
+    const updatedCart = {
+      items: updatedCartItems,
+    };
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items?.map((i) => i.productId) ?? [];
+
+    return db
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== productId.toString();
+    });
+
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      );
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection('users')
+      .findOne({ _id: new ObjectId(userId) })
+      .then((user) => {
+        return user;
+      })
+      .catch((err) => console.log(err));
+  }
+}
+
+module.exports = User;
