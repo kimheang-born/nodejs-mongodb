ad5b9fabdddabebaf80bc116fe62d78e5ff5c6a3
Kimheang <kimheang135@gmail.com>
2023-10-08 22:06:28 +0700

    fix: the wrong name

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('65226d84d2c28a6fd80ec2d3')
     .then((user) => {
-      req.user = new User(user.username, user.email, user.cart, user._id);
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
